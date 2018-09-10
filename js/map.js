8aafa9398df308e8ad7b3d0d7547d3aef03ab1e3
justedoit <justedoit92@gmail.com>
2018-09-10 13:39:22 +0300

    дз 1,2,3 пункт(правка)

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 var NUMBER_OF_OFFERS = 8;
@@ -8,20 +9,21 @@ var CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-
-function getRandomIndex(number) {
-  return Math.floor(Math.random() * number);
+// функция, возвращающая рандомный индекс массива
+var getRandomElement = function(arr) {
+return arr[Math.random() * arr.length]
 }
 
+// ф-ция случайного числа в заданном промежутке
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var shuffleArray = function (array) { 
+var shuffleArray = function (array) { // ф-ция, которая мешает массив
   var arrayCopy = array.slice();
   var mixedArray = [];
   while (mixedArray.length < array.length) {
-    var randomIndex = getRandomIndex(arrayCopy.length);
+    var randomIndex = getRandomElement(arrayCopy.length);
     mixedArray.push(arrayCopy[randomIndex]);
     arrayCopy.splice(randomIndex, 1);
   }
@@ -31,13 +33,13 @@ var shuffleArray = function (array) {
 var getOfferObject = function (index) {
   var titlesArrayCopy = TITLES.slice();
 
-  var randomIForTitlesArrayCopy = getRandomIndex(titlesArrayCopy.length);
-  var randomIForTypes = getRandomIndex(TYPES_OF_HOUSE.length);
-  var randomIForCheckin = getRandomIndex(CHECKIN_TIME.length);
-  var randomIForCheckout = getRandomIndex(CHECKOUT_TIME.length);
+  var randomIForTitlesArrayCopy = getRandomElement(titlesArrayCopy.length);
+  var randomIForTypes = getRandomElement(TYPES_OF_HOUSE.length);
+  var randomIForCheckin = getRandomElement(CHECKIN_TIME.length);
+  var randomIForCheckout = getRandomElement(CHECKOUT_TIME.length);
 
   var featuresArrayShuffleCopy = shuffleArray(FEATURES);
-  featuresArrayShuffleCopy.splice(0, getRandomIndex(featuresArrayShuffleCopy.length));
+  featuresArrayShuffleCopy.splice(0, getRandomElement(featuresArrayShuffleCopy.length));
 
   var mixedPhotosArray = shuffleArray(PHOTOS);
   var randomX = getRandomNumber(300, 900);
@@ -47,7 +49,7 @@ var getOfferObject = function (index) {
       'avatar': 'img/avatars/user0' + (index + 1) + '.png'
     },
     'offer': {
-      'title': titlesArrayCopy.splice(randomIForTitlesArrayCopy, 1)[0], 
+      'title': titlesArrayCopy.splice(randomIForTitlesArrayCopy, 1)[0], // беру рандомное значение из копии массива для текущего свойства объекта и удаляю его из копии массива
       'address': randomX + ',' + randomY,
       'price': getRandomNumber(1000, 1000000),
       'type': TYPES_OF_HOUSE[randomIForTypes],
@@ -70,11 +72,12 @@ var offers = [];
 for (var i = 0; i < NUMBER_OF_OFFERS; i++) {
   offers.push(getOfferObject(i));
 }
-
-
+// убираю класс у блока
 document.querySelector('.map').classList.remove('map--faded');
 
+// создаю метку на карте
 var mapPins = document.querySelector('.map__pins');
+
 
 var createButtonElement = function (object) {
   var newButtonElement = document.createElement('button');
@@ -92,6 +95,7 @@ var createButtonElement = function (object) {
   return newButtonElement;
 };
 
+// добиваюсь нужного мне количества меток при помощи ф-ции с циклом внутри, вставляю каждую во фрагмент и далее - в DOM
 var renderPins = function () {
   var documentFragment = document.createDocumentFragment();
   for (i = 0; i < offers.length; i++) {
@@ -100,8 +104,11 @@ var renderPins = function () {
   mapPins.appendChild(documentFragment);
 };
 
+//  dom-элемент объяления
+
 var offerCardTemplate = document.querySelector('template').content.querySelector('article.map__card');
 
+// ф-ция, проверяющая тип жилища
 var getTypeOfHouse = function (offerType) {
   var typeOfHouse;
   switch (offerType) {
@@ -119,7 +126,7 @@ var getTypeOfHouse = function (offerType) {
   }
   return typeOfHouse;
 };
-
+//  ф-ция манипуляций с фичами
 var getFeaturesArrayElement = function (featuresElement, offerFeaturesArray) {
 
   // удаляю иконки из шаблона, которые идут по умолчанию
@@ -138,7 +145,19 @@ var getFeaturesArrayElement = function (featuresElement, offerFeaturesArray) {
   featuresElement.appendChild(documentFragment);
 };
 
-var featuresElement = authorOfferCardElement.querySelector('.popup__features');
+var renderOfferCard = function (object) {
+  var authorOfferCardElement = offerCardTemplate.cloneNode(true);
+  authorOfferCardElement.querySelector('h3').textContent = object.offer.title;
+  authorOfferCardElement.querySelector('small').textContent = object.offer.address;
+  authorOfferCardElement.querySelector('.popup__price').innerHTML = object.offer.price + '&#x20bd;/ночь';
+  // в блок h4 вывожу тип жилья через вызов ф-ции типа дома
+  authorOfferCardElement.querySelector('h4').textContent = getTypeOfHouse(object.offer.type);
+  authorOfferCardElement.children[6].textContent = object.offer.rooms + ' комнаты для ' + object.offer.guests + ' гостей';
+  authorOfferCardElement.children[7].textContent = 'Заезд после ' + object.offer.checkin + ' , выезд до ' + object.offer.checkout;
+  authorOfferCardElement.children[9].textContent = object.offer.description;
+
+  // вызов ф-ции фич
+  var featuresElement = authorOfferCardElement.querySelector('.popup__features');
   getFeaturesArrayElement(featuresElement, object.offer.features);
   // удаляю строку <li> из шаблона
   var picturesElement = authorOfferCardElement.querySelector('.popup__pictures');
